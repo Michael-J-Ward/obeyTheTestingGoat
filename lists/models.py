94bdfbb3bf80958cbdaacccce8c5ee202c2790e3
Michael J Ward <ward.michael.j@gmail.com>
2016-07-22 15:12:56 -0500

    added requirements.txt

--- lists/models.py
+++ lists/models.py
@@ -1,10 +1,14 @@
+"Fundamental models for lists"
+
 from django.db import models
 
 # Create your models here.
 
 class List(models.Model):
+	"""The basic list model"""
 	pass
 
 class Item(models.Model):
+	"""The item that goes into lists"""
 	text = models.TextField(default='')
 	list = models.ForeignKey(List, default=None)
