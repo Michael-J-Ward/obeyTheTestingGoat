f8d49a2eb833f1d7415b6e9022d7c48a6ca031bd
Michael J Ward <ward.michael.j@gmail.com>
2016-07-26 10:09:12 -0500

    blah

--- check_circleCI_config.sh
+++ check_circleCI_config.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 # This script checks that CircleCI can publish to github
 # navigate to https://username.github.io/project-name/
+mkdir ~/gh_docs
+cd ~/gh_docs
 
-git checkout gh-pages
+git clone -b gh-pages "$CIRCLE_REPOSITORY_URL.git" html
+cd html
+
 rm indext.html
 echo "I was created by circleCI!" > index.html
+
 git remote set-url origin git@gist.github.com:Michael-J-Ward/obeyTheTestingGoat
 git add index.html
 git commit -m "creating and publishing docs from circleCI"
