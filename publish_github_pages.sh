277af6092f144a0abbaba92c528a346a0fde9163
Michael J Ward <ward.michael.j@gmail.com>
2016-07-25 19:51:26 -0500

    cross your fingers

--- publish_github_pages.sh
+++ publish_github_pages.sh
@@ -1,8 +1,25 @@
 #! /bin/bash
 
-git checkout gh-pages
-rm indext.html
-echo "I was created by circleCI!" > index.html
-git add index.html
-git commit -m "creating and publishing docs from circleCI"
+# It's easiest to work in a directory not under version control
+mkdir ~/gh_docs
+cd ~/gh_docs
+
+git clone -b gh-pages "$CIRCLE_REPOSITORY_URL.git" html
+cd html
+
+# Clean the folder 
+git rm -rf .
+git commit -m "cleaning directory for document generation"
+
+# Generate the docs
+cd ~/lucro/api/docs
+make html
+
+mv _build/html/* ~/gh_docs/html
+cd ~/gh_docs/html
+
+touch .nojekyll
+
+git add .
+git commit -m "auto-creating and publishing docs"
 git push origin gh-pages
